9f515d12c547f3916087bddb63a6e16394c64627
Chirsth Peralta <christhperalta@gmail.com>
2023-03-21 14:23:25 -0400

    Adding Description,Botton and Icon components

--- src/provider/Provider.jsx
+++ src/provider/Provider.jsx
@@ -7,15 +7,12 @@ const data = {
     title: 'PERFUME',
     brand: 'Gabrielle Essence Eau De Parfum',
     text: 'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
-    price: '$149.99',
+    currentPrice: '$149.99',
     previousPrice: '$149.99',
-    desktopImg: '../assets/images/image-product-desktop.jpg',
-    mobileImg: '../assets/images/image-product-mobile.jpg',
   },
 
   btn: {
     text: 'Add to Cart',
-    src: '../assets/images/icon-cart.svg',
   },
 };
 export const GlobalContext = createContext();
